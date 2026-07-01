ccac94660902fc2e9a53261a6fbf600e919077be
Noor <noor-al-abri@hotmail.com>
2026-07-01 09:14:18 +0400

    Task 1 - Personal Info Card

---
+++ SparkToCode2026/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SparkToCode2026;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1 - Personal Info Card
+
+        string name= "sara";
+        int age =21 ;
+        double height = 1.65;
+        bool student =true;
+        
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Student: " + student);
+        
+
+    }
+}
