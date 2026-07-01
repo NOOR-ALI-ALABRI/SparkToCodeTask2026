6fca811a61620af0085ebc733804f8235a5ebdfa
Noor <noor-al-abri@hotmail.com>
2026-07-01 12:51:43 +0400

    Task 9 - Day Name Finder

--- SparkToCode2026/Program.cs
+++ SparkToCode2026/Program.cs
@@ -148,6 +148,49 @@ class Program
       Console.WriteLine("Original bill: " + bill);
       Console.WriteLine("Discount: " + discount);
       Console.WriteLine("Final amount: " + finalAmount);
+      //////////////////////////////////////////////////
+      //Task 9 - Day Name Finder
+      Console.WriteLine("Enter a number from 1 to 7: ");
+      int num = int.Parse(Console.ReadLine());
+      switch (num)
+      {
+          case 1:
+              Console.WriteLine("Sunday");
+              break;
+
+          case 2:
+              Console.WriteLine("Monday");
+              break;
+
+          case 3:
+              Console.WriteLine("Tuesday");
+              break;
+
+          case 4:
+              Console.WriteLine("Wednesday");
+              break;
+
+
+          case 5:
+              Console.WriteLine("Thursday");
+              break;
+          
+          case 6:
+              Console.WriteLine("Friday");
+              break;
+          
+          case 7:
+              Console.WriteLine("Saturday");
+              break;
+          
+          default:
+              Console.WriteLine("Invalid day number");
+              break;
+      }
+
+
+      //////////////////////
+      /// 
       
     }
 }
